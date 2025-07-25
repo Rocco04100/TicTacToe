18838673292380ad4cddfd4116563e55c882dff7
Rocco <rocco.demartino25@gmail.com>
2025-07-25 14:06:02 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const newGameBtnE = document.querySelector(`#newGame`);
+
+const topLeftBtnE = document.querySelector(`.left .top button`);
+const middleLeftBtnE = document.querySelector(`.left .middle button`);
+const bottomLeftBtnE = document.querySelector(`.left .bottom button`);
+
+const topmMiddleBtnE = document.querySelector(`.middle .top button`);
+const middleMiddleBtnE = document.querySelector(`.middle .middle button`);
+const bottomMiddleBtnE = document.querySelector(`.middle .bottom button`);
+
+const topmRightBtnE = document.querySelector(`.right .top button`);
+const middleRightBtnE = document.querySelector(`.right .middle button`);
+const bottomRightBtnE = document.querySelector(`.right .bottom button`);
+
+function game() {
+    
+}
